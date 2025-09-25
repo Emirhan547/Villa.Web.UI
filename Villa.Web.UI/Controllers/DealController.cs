b0827b232541ae3a34b9836f8fc68322c5c53d9a
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-09-25 18:19:02 +0300

    Deal Controller ve View tamamlandı.Silme işlemi için Sweet Alert eklendi

---
+++ Villa.Web.UI/Controllers/DealController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.CounterDtos;
+using Villa.Dto.Dtos.DealDtos;
+using Villa.Entity.Entities;
+
+namespace Villa.Web.UI.Controllers
+{
+    public class DealController : Controller
+    {
+        private readonly IDealService _dealService;
+        private readonly IMapper _mapper;
+
+        public DealController(IDealService dealService, IMapper mapper)
+        {
+            _dealService = dealService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _dealService.TGetListAsync();
+            var dealList = _mapper.Map<List<ResultDealDto>>(values);
+            return View(dealList);
+        }
+        public async Task<IActionResult> DeleteDeal(ObjectId id)
+        {
+            await _dealService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+        public IActionResult CreateDeal()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateDeal(CreateDealDto createDealDto)
+        {
+            var newDeal = _mapper.Map<Deal>(createDealDto);
+            await _dealService.TCreateAsync(newDeal);
+            return RedirectToAction("Index");
+
+        }
+        public async Task<IActionResult> UpdateDeal(ObjectId id)
+        {
+            var value = await _dealService.TGetByIdAsync(id);
+            var updateDeal = _mapper.Map<UpdateDealDto>(value);
+            return View(updateDeal);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateDeal(UpdateDealDto updatedDealDto)
+        {
+            var updateDeal = _mapper.Map<Deal>(updatedDealDto);
+            await _dealService.TUpdateAsync(updateDeal);
+            return RedirectToAction("Index");
+        }
+    }
+}
