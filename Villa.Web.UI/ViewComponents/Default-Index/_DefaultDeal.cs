1c878484fdaf7d4d331b2a7750e8b74494161349
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-09-29 18:47:13 +0300

    UI tarafında Counter ,Deal,Product,Contact kısımları ViewComponentlere ayrıldı ve düzenlendi

---
+++ Villa.Web.UI/ViewComponents/Default-Index/_DefaultDeal.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Business.Abstract;
+using Villa.Dto.Dtos.DealDtos;
+
+namespace Villa.Web.UI.ViewComponents.Default_Index
+{
+    public class _DefaultDeal:ViewComponent
+    {
+        private readonly IDealService _dealService;
+        private readonly IMapper _mapper;
+
+        public _DefaultDeal(IDealService dealService, IMapper mapper)
+        {
+            _dealService = dealService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var deals = await _dealService.TGetListAsync();
+            var mappedDeals = _mapper.Map<List<ResultDealDto>>(deals);
+            return View(mappedDeals);
+        }
+    }
+}
---
+++ Villa.Web.UI/ViewComponents/Default-Index/_DefaultMessage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.Web.UI.ViewComponents.Default_Index
+{
+    public class _DefaultMessage:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
